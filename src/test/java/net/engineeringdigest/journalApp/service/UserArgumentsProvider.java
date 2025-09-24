90f810ff02c8b09954c44f319b26e4ea9f9db63a
Partha-Dey001 <deyp9187@gmail.com>
2025-09-24 17:12:40 +0530

    first commit

---
+++ src/test/java/net/engineeringdigest/journalApp/service/UserArgumentsProvider.java
@@ -0,0 +1,23 @@
+package net.engineeringdigest.journalApp.service;
+
+import net.engineeringdigest.journalApp.entity.User;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+
+
+import java.util.stream.Stream;
+
+@Disabled
+public class UserArgumentsProvider implements ArgumentsProvider {
+
+    @Override
+    public Stream<? extends Arguments> provideArguments(ExtensionContext extensionContext) throws Exception {
+        return Stream.of(
+                Arguments.of(new User("shyam", "shyam")),
+                Arguments.of(new User ("vicky", "vicky")),
+                Arguments.of(new User ("ramu", "ramu"))
+        );
+    }
+}
