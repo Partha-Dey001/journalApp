81bb1d49e55679ab0b504b7d6a52911a1d9ddf61
Partha-Dey001 <deyp9187@gmail.com>
2025-10-28 20:38:27 +0530

    fix: minor changes in test files

--- src/test/java/net/engineeringdigest/journalapp/service/EmailServiceTests.java
+++ src/test/java/net/engineeringdigest/journalapp/service/EmailServiceTests.java
@@ -1,5 +1,6 @@
 package net.engineeringdigest.journalapp.service;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,6 +13,7 @@ public class EmailServiceTests {
     private EmailService emailService;
 
     @Test
+    @Disabled
     public void sendEmailTest () {
         emailService.sendEmail("parthadeyofficial1@gmail.com",
                 "Testing java mail sender",
